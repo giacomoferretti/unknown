0b28b9e31bbd46ff5c550b683a950e4d4ff29ad1
Oliviero Petrucci <olivieropetrucci@gmail.com>
2022-11-12 04:08:33 +0100

    sium

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -19,7 +19,7 @@ const QrCode = ({ input }: { input: string }) => {
     <>
       {data.length !== 0 && (
         <Image
-          className="rounded-md border shadow-md"
+          className="h-[6.8rem] w-[6.8rem] "
           alt=""
           src={data}
           width={200}
@@ -48,25 +48,29 @@ const Dashboard: NextPage = () => {
         <title>{`Dashboard | ${env.NEXT_PUBLIC_EXPERIENCE_NAME}`}</title>
       </Head>
 
-      <div className="mx-auto flex h-screen max-w-4xl flex-col items-center justify-center py-16">
-        <h1 className="flex flex-col text-center text-4xl font-extrabold">
+      <div className="mx-auto flex h-screen w-screen flex-col items-center justify-center bg-white py-16 text-stone-800">
+        <h1 className="flex flex-col text-center text-5xl font-extrabold">
           {env.NEXT_PUBLIC_EXPERIENCE_NAME}{" "}
-          <span className="p-2 text-lg font-normal">
+          <span className="p-2 text-xl font-normal">
             - Campaign Analytics -
           </span>
         </h1>
-        <div className="mx-auto mt-20 flex h-32 w-80 items-center justify-between rounded-full border border-neutral-500 p-6">
-          <h2 className="ml-4 text-xl font-bold text-stone-800">
-            QR Code visits
-          </h2>
-          <div className="flex h-20 w-20 items-center justify-center rounded-full border-8 border-neutral-500">
-            <p className="text-2xl font-extrabold">
-              {hello.isLoading && <>Loading...</>}
-              {hello.data && <>{hello.data.hits.toString()}</>}
-            </p>
+        <div className="mt-20 flex flex-row items-center justify-center">
+          <div className="mx-auto  flex h-32 w-80 items-center justify-between rounded-full border-2 border-stone-800 p-6">
+            <h2 className="ml-4 text-xl font-bold text-stone-800">
+              QR Code visits
+            </h2>
+            <div className="flex h-20 w-20 items-center justify-center rounded-full border-8 border-stone-500">
+              <p className="text-2xl font-extrabold">
+                {hello.isLoading && <>Loading...</>}
+                {hello.data && <>{hello.data.hits.toString()}</>}
+              </p>
+            </div>
+          </div>
+          <div className=" mx-4 flex h-[10rem] w-[10rem] items-center justify-center rounded-full p-2">
+            {qr && <QrCode input={qr} />}
           </div>
         </div>
-        {qr && <QrCode input={qr} />}
       </div>
     </>
   );
